4e76dab4a15111c0631e5b4fae7a4651a39645f7
windstep <icq431@yandex.ru>
2022-04-07 16:39:23 +0300

    fix: client

--- services/send_message.js
+++ services/send_message.js
@@ -2,6 +2,26 @@ const output = require("../output.js");
 const socket = require("../socket.js");
 const disk = require("../disk.js");
 const discord = require('discord.js')
+const client = new discord.Client({
+    ws: {
+        intents: [
+            "DIRECT_MESSAGES",
+            "DIRECT_MESSAGE_REACTIONS",
+            "DIRECT_MESSAGE_TYPING",
+            "GUILDS",
+            "GUILD_BANS",
+            "GUILD_EMOJIS",
+            "GUILD_INTEGRATIONS",
+            "GUILD_INVITES",
+            "GUILD_MEMBERS",
+            "GUILD_MESSAGES",
+            "GUILD_MESSAGE_REACTIONS",
+            "GUILD_MESSAGE_TYPING",
+            "GUILD_VOICE_STATES",
+            "GUILD_WEBHOOKS"
+        ]
+    }
+});
 
 /**
  *
